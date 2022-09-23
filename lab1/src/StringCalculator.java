1c45dfdaf0d4687b6e21b1a7949589e4e70ce5da
Vladimir Patsaluk <vpatsaluk.gmail.com>
2022-09-24 00:16:29 +0300

    update

--- lab1/src/StringCalculator.java
+++ lab1/src/StringCalculator.java
@@ -1,146 +1,116 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.stream.Collectors;
-
+import java.util.List;
+import java.util.Scanner;
 public class StringCalculator {
 
-    public static int add(String numbers){
+    int add(String numbers){
         if(numbers.isEmpty()) return 0;
-        else{
+
+        if(numbers.contains("\\n") || numbers.contains(",")){
+            numbers = numbers.replace("\\n", "\n");
+
+            List<String> delimitersArray = new ArrayList<>();
+
             if(numbers.startsWith("//")){
-                String delimiter;
-                int newLineIndex = numbers.indexOf("\n");
+                String[] numbersParts = numbers.split("\n");
 
-                ArrayList<String> numbersArrayList = new ArrayList<>();
-                ArrayList<String> negativeNumbersArray = new ArrayList<>();
-                ArrayList<String> delimitersList = new ArrayList<>();
+                numbersParts[0] = numbersParts[0].substring(2);
 
-                String[] numbersStringArray = {"0"};
+                int index_in = 0;
+                int index_out = 0;
+                for(int i = 0; i < numbersParts[0].length(); i++){
+                    if((numbersParts[0].charAt(i) == '[' && i == 0) || (numbersParts[0].charAt(i - 1) != '[' && numbersParts[0].charAt(i) == '['))
+                        index_in = i + 1;
 
-                String delimiterPart = numbers.substring(2, newLineIndex);
-                char[] delimiterArray = delimiterPart.toCharArray();
-                String del = "";
-
-                for(int i = 0; i < delimiterArray.length; i++){
-                    if(delimiterArray[i] == ']'){
-                        delimitersList.add(del);
-                        del = "";
-                    }
-                    if(delimiterArray[i] != '[' && delimiterArray[i] != ']')
-                        del = del.concat(Character.toString(delimiterArray[i]));
-
-                }
-
-                String[] delArray = new String[delimitersList.size()];
-                delimitersList.toArray(delArray);
-
-                System.out.println(delimitersList);
-
-                if(numbers.charAt(newLineIndex - 1) == ']'){
-                    delimiter = numbers.substring(newLineIndex - 2, newLineIndex - 1);
-
-                    numbers = numbers.substring(newLineIndex + 1);
-
-                    int delimiterInd;
-                    int minDelInd = delArray.length + 1;
-
-                    while(!numbers.isEmpty()){
-
-                        for(int i = 0; i < delArray.length; i++){
-                            if(numbers.contains(delArray[i])){
-                                if(minDelInd > numbers.indexOf(delArray[i])) minDelInd = numbers.indexOf(delArray[i]);
-                            }
-                            if(i == delArray.length - 1){
-                                if(minDelInd != 0) numbersArrayList.add(numbers.substring(0, minDelInd));
-                            }
-                            else if(i == delArray.length - 1 && numbers.contains(numbers.charAt())){
-
-                            }
-                        }
-
-                        if(numbers.contains()){
-                            delimiterInd = numbers.indexOf(delimiter);
-                            if(delimiterInd != 0) numbersArrayList.add(numbers.substring(0, delimiterInd));
-                        }
-                        else{
-                            numbersArrayList.add(numbers);
-                            break;
-                        }
-                        numbers = numbers.substring(delimiterInd + 1);
-
-                    }
-
-                    numbersStringArray = new String[numbersArrayList.size()];
-                    numbersArrayList.toArray(numbersStringArray);
-
-                }
-                else {
-                    delimiter = numbers.substring(newLineIndex - 1, newLineIndex);
-
-                    numbers = numbers.substring(newLineIndex + 1);
-
-                    numbersStringArray = numbers.split(delimiter);
-                }
-
-                int sum = 0;
-
-                for(int i = 0; i < numbersStringArray.length; i++){
-                    if(Integer.parseInt(numbersStringArray[i]) < 0){
-                        negativeNumbersArray.add(numbersStringArray[i]);
-                    }
-
-                    if(Integer.parseInt(numbersStringArray[i]) >= 0 && Integer.parseInt(numbersStringArray[i]) <= 1000){
-                        sum += Integer.parseInt(numbersStringArray[i]);
+                    else if((numbersParts[0].charAt(i) == ']' && i == numbersParts[0].length() - 1) || (numbersParts[0].charAt(i) == ']' && numbersParts[0].charAt(i + 1) != ']')){
+                        index_out = i - 1;
+                        delimitersArray.add(numbersParts[0].substring(index_in, index_out + 1));
                     }
                 }
 
-                if(negativeNumbersArray.isEmpty()) return sum;
-                else{
-                    throw  new IllegalArgumentException("Недозволені від'ємні числа - " + negativeNumbersArray.stream().map(Object::toString).collect(Collectors.joining(", ")));
+                numbers = numbers.substring(numbers.indexOf("\n") + 1);
+            }
+
+            delimitersArray.add(",");
+            delimitersArray.add("\n");
+
+            List<String> delimitersToCheck = new ArrayList<>();
+
+            String tempDel = "";
+            for(int i = 0; i < numbers.length(); i++){
+                if(!Character.toString(numbers.charAt(i)).equals("-")) {
+                    if (!isNumeric(Character.toString(numbers.charAt(i))))
+                        tempDel = tempDel.concat(Character.toString(numbers.charAt(i)));
+
+                    if (isNumeric(Character.toString(numbers.charAt(i))) || i == numbers.length() - 1) {
+                        delimitersToCheck.add(tempDel);
+                        tempDel = "";
+                    }
                 }
             }
 
-            else if(numbers.contains(",\n") || numbers.contains("\n,")){
-                throw new IllegalArgumentException("Використання коми та нової строки як розділювача одночасно");
-            }
-
-            else if(numbers.contains(",") || numbers.contains("\n")){
-
-                String[] numbersStringArray = numbers.split(",|\n");
-
-                int sum = 0;
-
-                ArrayList<String> negativeNumbersArray = new ArrayList<>();
-
-                for(int i = 0; i < numbersStringArray.length; i++){
-                    if(Integer.parseInt(numbersStringArray[i]) < 0){
-                        negativeNumbersArray.add(numbersStringArray[i]);
-                    }
-
-                    if(Integer.parseInt(numbersStringArray[i]) >= 0 && Integer.parseInt(numbersStringArray[i]) <= 1000){
-                        sum += Integer.parseInt(numbersStringArray[i]);
-                    }
-                }
-
-                if(negativeNumbersArray.isEmpty()) return sum;
-                else{
-                    throw  new IllegalArgumentException("Недозволені від'ємні числа - " + negativeNumbersArray.stream().map(Object::toString).collect(Collectors.joining(", ")));
+            for(int i = 0; i < delimitersToCheck.size(); i++){
+                if(!delimitersArray.contains(delimitersToCheck.get(i)) && !delimitersToCheck.get(i).equals("")){
+                    throw new IllegalArgumentException("Помилка: Два роздільника підряд або неіснуючий роздільник");
                 }
             }
+
+            List<String> numbersArray = new ArrayList<>();
+
+            while(!numbers.isEmpty()){
+                for(int i = 0; i < delimitersArray.size(); i++){
+                    String[] temp = numbers.split("\\Q" + delimitersArray.get(i) + "\\E");
+                    if(isNumeric(temp[0])){
+                        numbersArray.add(temp[0]);
+                        if(temp[0].length() + delimitersArray.get(i).length() <= numbers.length())
+                            numbers = numbers.substring(temp[0].length() + delimitersArray.get(i).length());
+                        else
+                            numbers = "";
+                    }
+                }
+            }
+
+            int sum = 0;
+            List<Integer> negativeNumbers = new ArrayList<>();
+            for(int i = 0; i < numbersArray.size(); i++){
+                if (Integer.parseInt(numbersArray.get(i)) < 0)
+                    negativeNumbers.add(Integer.parseInt(numbersArray.get(i)));
+                if (Integer.parseInt(numbersArray.get(i)) <= 1000)
+                    sum += Integer.parseInt(numbersArray.get(i));
+            }
+
+            if(negativeNumbers.isEmpty()) return sum;
 
             else{
-                if(Integer.parseInt(numbers) < 0){
-                   throw  new IllegalArgumentException("Недозволені від'ємні числа - " + numbers);
-                }
-
-                if(Integer.parseInt(numbers) >= 0 && Integer.parseInt(numbers) <= 1000){
-                    return Integer.parseInt(numbers);
-                }
-                else return 0;
+                throw new IllegalArgumentException("Недозволені від'ємні числа: " + negativeNumbers);
             }
         }
-    }
 
+        if(Integer.parseInt(numbers) < 0)
+            throw new IllegalArgumentException("Недозволені від'ємні числа: " + numbers);
+        if(Integer.parseInt(numbers) > 1000) return 0;
+
+        return Integer.parseInt(numbers);
+    }
+    public static boolean isNumeric(String str){
+        try{
+            Integer.parseInt(str);
+            return true;
+        }
+        catch (NumberFormatException e){
+            return false;
+        }
+    }
+}
+
+class MainClass{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введіть рядок:");
+        String inputNumbers = sc.nextLine();
+
+        StringCalculator stringCalculator = new StringCalculator();
+        System.out.println(stringCalculator.add(inputNumbers));
+    }
 }
