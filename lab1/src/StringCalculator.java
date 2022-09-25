8fbcd388c3cb2680bf9d3db11fecda03baa13fe6
Vladimir Patsaluk <vpatsaluk.gmail.com>
2022-09-25 23:37:47 +0300

    update

--- lab1/src/StringCalculator.java
+++ lab1/src/StringCalculator.java
@@ -1,89 +1,79 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 public class StringCalculator {
 
     int add(String numbers){
         if(numbers.isEmpty()) return 0;
 
-        if(numbers.contains("\\n") || numbers.contains(",")){
+        if(numbers.contains("\\n") || numbers.contains(",")) {
             numbers = numbers.replace("\\n", "\n");
 
             List<String> delimitersArray = new ArrayList<>();
 
-            if(numbers.startsWith("//")){
-                String[] numbersParts = numbers.split("\n");
+            if (numbers.startsWith("//")) {
+                numbers = numbers.substring(2);
 
-                numbersParts[0] = numbersParts[0].substring(2);
+                String[] temp1 = numbers.split("]\n");
+
+                String delimiterPart = numbers.substring(0, temp1[0].length() + 1);
 
                 int index_in = 0;
                 int index_out = 0;
-                for(int i = 0; i < numbersParts[0].length(); i++){
-                    if((numbersParts[0].charAt(i) == '[' && i == 0) || (numbersParts[0].charAt(i - 1) != '[' && numbersParts[0].charAt(i) == '['))
+                for (int i = 0; i < delimiterPart.length(); i++) {
+                    if ((delimiterPart.charAt(i) == '[' && i == 0) || (delimiterPart.charAt(i - 1) != '[' && delimiterPart.charAt(i) == '['))
                         index_in = i + 1;
 
-                    else if((numbersParts[0].charAt(i) == ']' && i == numbersParts[0].length() - 1) || (numbersParts[0].charAt(i) == ']' && numbersParts[0].charAt(i + 1) != ']')){
+                    else if ((delimiterPart.charAt(i) == ']' && i == delimiterPart.length() - 1) || (delimiterPart.charAt(i) == ']' && delimiterPart.charAt(i + 1) != ']')) {
                         index_out = i - 1;
-                        delimitersArray.add(numbersParts[0].substring(index_in, index_out + 1));
+                        delimitersArray.add(delimiterPart.substring(index_in, index_out + 1));
                     }
                 }
-
-                numbers = numbers.substring(numbers.indexOf("\n") + 1);
+                numbers = temp1[1];
             }
 
             delimitersArray.add(",");
             delimitersArray.add("\n");
 
-            List<String> delimitersToCheck = new ArrayList<>();
-
-            String tempDel = "";
-            for(int i = 0; i < numbers.length(); i++){
-                if(!Character.toString(numbers.charAt(i)).equals("-")) {
-                    if (!isNumeric(Character.toString(numbers.charAt(i))))
-                        tempDel = tempDel.concat(Character.toString(numbers.charAt(i)));
-
-                    if (isNumeric(Character.toString(numbers.charAt(i))) || i == numbers.length() - 1) {
-                        delimitersToCheck.add(tempDel);
-                        tempDel = "";
+            for (int i = 0; i < delimitersArray.size() - 1; ++i) {
+                for (int j = 0; j < delimitersArray.size() - i - 1; ++j) {
+                    if (delimitersArray.get(j + 1).length() > delimitersArray.get(j).length()) {
+                        String swap = delimitersArray.get(j + 1);
+                        delimitersArray.set(j + 1, delimitersArray.get(j));
+                        delimitersArray.set(j, swap);
                     }
                 }
             }
 
-            for(int i = 0; i < delimitersToCheck.size(); i++){
-                if(!delimitersArray.contains(delimitersToCheck.get(i)) && !delimitersToCheck.get(i).equals("")){
-                    throw new IllegalArgumentException("Помилка: Два роздільника підряд або неіснуючий роздільник");
-                }
+            for (int i = 0; i < delimitersArray.size(); i++) {
+                numbers = numbers.replaceAll("\\Q" + delimitersArray.get(i) + "\\E", ",");
             }
 
-            List<String> numbersArray = new ArrayList<>();
+            if (numbers.contains(",,"))
+                throw new IllegalArgumentException("Помилка: Два дільники підряд або невідомий дільник");
 
-            while(!numbers.isEmpty()){
-                for(int i = 0; i < delimitersArray.size(); i++){
-                    String[] temp = numbers.split("\\Q" + delimitersArray.get(i) + "\\E");
-                    if(isNumeric(temp[0])){
-                        numbersArray.add(temp[0]);
-                        if(temp[0].length() + delimitersArray.get(i).length() <= numbers.length())
-                            numbers = numbers.substring(temp[0].length() + delimitersArray.get(i).length());
-                        else
-                            numbers = "";
-                    }
-                }
-            }
+            String[] numbersArray = numbers.split(",");
 
             int sum = 0;
-            List<Integer> negativeNumbers = new ArrayList<>();
-            for(int i = 0; i < numbersArray.size(); i++){
-                if (Integer.parseInt(numbersArray.get(i)) < 0)
-                    negativeNumbers.add(Integer.parseInt(numbersArray.get(i)));
-                if (Integer.parseInt(numbersArray.get(i)) <= 1000)
-                    sum += Integer.parseInt(numbersArray.get(i));
+            List<String> negativeNumbers = new ArrayList<>();
+
+            for (int i = 0; i < numbersArray.length; i++) {;
+                if (isNumeric(numbersArray[i])){
+                    if (Integer.parseInt(numbersArray[i]) < 1001)
+                        sum += Integer.parseInt(numbersArray[i]);
+                    if (Integer.parseInt(numbersArray[i]) < 0)
+                        negativeNumbers.add(numbersArray[i]);
+
+                }
+                else
+                    throw new IllegalArgumentException("Помилка: Два дільники підряд або невідомий дільник");
             }
 
-            if(negativeNumbers.isEmpty()) return sum;
-
-            else{
+            if (!negativeNumbers.isEmpty())
                 throw new IllegalArgumentException("Недозволені від'ємні числа: " + negativeNumbers);
-            }
+
+            return sum;
         }
 
         if(Integer.parseInt(numbers) < 0)
